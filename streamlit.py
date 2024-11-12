8f777067d668b1001ef215ea85646c41e87a5896
NISHANT SINGH TOMAR <nishanttomar98@gmail.com>
2024-11-12 17:44:23 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,7 @@ import openai
 from llama_index.llms.openai import OpenAI
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Rag based bot for GPMC using Llamaindex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
 st.title("Rag Based Bot for The Ahmedabad Municipal Corporation")
 
